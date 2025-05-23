7bfc80e4cfe2021571ec55afea03a96f0fcb6162
NATA SANTOS DOS REIS <nata.sreis@senacsp.edu.br>
2025-05-23 15:37:01 -0300

    first commit

--- salario.js
+++ salario.js
@@ -7,10 +7,13 @@ function calcular(){
 
     const valorComissao = (comissao / 100) * vendas
     const salarioBruto = salario + valorComissao
+    const inss = (8 / 100) * salarioBruto
     const salarioLiquido = salarioBruto - ((8 / 100) * salarioBruto)
 
     resultado.innerHTML = `<p>${funcionario} suas seguintes informações são: <br>
+     Salário base: ${salario} <br>
      Valor da comissão: ${valorComissao.toFixed(2)} <br>
      Salário bruto: ${salarioBruto.toFixed(2)} <br>
+     desconto do INSS: ${inss} <br>
      Salário líquido (8% INSS): ${salarioLiquido.toFixed(2)}</p>`
 }
